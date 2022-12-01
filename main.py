515001928fb98a3dbb67e79160085c0228c6ba09
Valentina Franco <valentina.franco@udea.edu.co>
2022-12-01 14:25:50 -0500

    New

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ import json
 
 app = FastAPI()
 
-file='https://raw.githubusercontent.com/valentinafv96/SecExam/main/data/openalexco_udea_pp_reduced.json'
+file='https://raw.githubusercontent.com/valentinafv96/SecondPart/main/data/openalexco_udea_pp_reduced.json'
 
 #JSON SCHEME
 #[{"student_id": str,
